79132821e3fc09ed865b95148cbc4b425da0b1b9
Kareem Hage-Ali <alikare4@IITS-I406-21.utsc-labs.utoronto.ca>
2018-03-15 15:48:41 -0400

    Frontend updated with react-bootstrap

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button bsStyle="primary">hello</Button>
       </div>
     );
   }

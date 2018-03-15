bf3a43890ea56e14cbd18edf27c9e626a5892dc7
ralphambegia <raphael.ambegia24@gmail.com>
2018-03-15 17:51:29 -0400

    DAW

--- tellmestuff/urls.py
+++ tellmestuff/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [

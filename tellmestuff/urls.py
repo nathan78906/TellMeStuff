6174e1635b208d3fd9f6a1405cb71a26f9f01961
ralphambegia <raphael.ambegia24@gmail.com>
2018-03-15 17:58:07 -0400

    DAW

--- tellmestuff/urls.py
+++ tellmestuff/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^backend/', include('backend.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    url('.*', TemplateView.as_view(template_name='index.html')),
 ]

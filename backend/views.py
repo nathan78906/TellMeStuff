5e8dcae7d003027777914e58ffd38460b8210628
nathan78906 <nathen78906@gmail.com>
2018-03-15 19:23:59 -0400

    csrf again???

--- backend/views.py
+++ backend/views.py
@@ -4,9 +4,11 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.decorators.http import require_POST
+from django.utils.decorators import method_decorator
 
 @csrf_exempt
 @require_POST
+@method_decorator(csrf_exempt)
 def dialogflow(request):
     json_data = request.body
     print(json_data)

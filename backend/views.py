a647d44c91df519b3b5a2215078e88a05ad916ca
ralphambegia <raphael.ambegia24@gmail.com>
2018-03-15 20:59:24 -0400

    fixed stuff

--- backend/views.py
+++ backend/views.py
@@ -6,11 +6,10 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.decorators.http import require_POST
 from django.utils.decorators import method_decorator
 
+@csrf_exempt
 @require_POST
 def dialogflow(request):
     json_data = request.body
-    print(json_data)
-
     response = JsonResponse({"messages": [{"imageUrl": "https://i.imgur.com/kmyWgqH.jpg","platform": "facebook", "type": 3}]})
 
     #response = JsonResponse({"facebook": {"attachment":{"type":"image", "payload":{"url":"https://i.imgur.com/kmyWgqH.jpg", "is_reusable":"true"}}}})

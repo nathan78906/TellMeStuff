5c008b538eb67a39245fdc822969b6cd01a811c4
ralphambegia <raphael.ambegia24@gmail.com>
2018-03-16 18:41:07 -0400

    taowkda

--- tellmestuff/urls.py
+++ tellmestuff/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
     url(r'^backend/', include('backend.urls')),
+    url(r'^', TemplateView.as_view(template_name="index.html")),
     #url('.*', TemplateView.as_view(template_name='index.html')),
 ]

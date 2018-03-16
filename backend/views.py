0db862a5b624c11dbef608c52dd9c8ebb79497c1
nathan78906 <nathen78906@gmail.com>
2018-03-16 19:01:06 -0400

    weather

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+from weather import Weather, Unit
 
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,7 +12,25 @@ def dialogflow(request):
     json_data = request.body
     print(json_data)
 
-    response = JsonResponse({"messages": [{"imageUrl": "https://i.imgur.com/kmyWgqH.jpg","platform": "facebook", "type": 3}]})
+    weather = Weather(unit=Unit.CELSIUS)
+
+    # Lookup via location name.
+
+    location = weather.lookup_by_location('toronto')
+    condition = location.condition()
+
+    # Get weather forecasts for the upcoming days.
+
+    string = location.location().city() + "," + location.location().country()
+    string += "\nCurrent Temp: " + condition.temp() + " C"
+
+    forecast = location.forecast()[0]
+    string += "\nCondition: " + (forecast.text())
+    string += "\nWith a high of " + forecast.high() + " C"
+    string += "\nand a low of " + forecast.low() + " C"
+
+    response = JsonResponse("messages": [{"platform": "facebook","speech": string,"type": 0}])
+    #response = JsonResponse({"messages": [{"imageUrl": "https://i.imgur.com/kmyWgqH.jpg","platform": "facebook", "type": 3}]})
 
     #response = JsonResponse({"facebook": {"attachment":{"type":"image", "payload":{"url":"https://i.imgur.com/kmyWgqH.jpg", "is_reusable":"true"}}}})
     return response
--- tellmestuff/urls.py
+++ tellmestuff/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^backend/', include('backend.urls')),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]

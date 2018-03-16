601cfa4a19e47b46a262d84608eed3231f93ab1f
ralphambegia <raphael.ambegia24@gmail.com>
2018-03-15 20:46:52 -0400

    fixing csrf

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt   
 
 from . import views
-#@csrf_exempt
+
 urlpatterns = [
-    url(r'^dialogflow/', csrf_exempt(views.dialogflow), name='dialogflow'),
+    url(r'^dialogflow/', views.dialogflow, name='dialogflow'),
 ]

38c24e54c39a71fa5d4d0817147b440fb8ab6210
kareemhageali <kareem.hageali@gmail.com>
2018-03-19 16:03:26 -0400

    Created tables for weather and profile

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Weather(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    location = models.CharField(max_length=50)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    phone_number = models.CharField(max_length=50, default=None)
+    facebook_id = models.CharField(max_length=100, default=None)
+    google_assistant_id = models.CharField(max_length=100, default=None)
+    slack_id = models.CharField(max_length=100, default=None)
+    twitter_id = models.CharField(max_length=100, default=None)
+        

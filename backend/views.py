b5fd20bdc8492adadd4cce60839c0f638267c67f
nathan78906 <nathen78906@gmail.com>
2018-03-19 21:06:20 -0400

    debugging

--- backend/views.py
+++ backend/views.py
@@ -23,14 +23,17 @@ def dialogflow(request):
         user_id = int(body["originalRequest"]["data"]["postback"]["referral"]["ref"])
         user = User.objects.get(pk=user_id)
         if Profile.objects.filter(user=user).exists():
-            entry = Weather.objects.get(user = request.user)
+            entry = Weather.objects.get(user=user)
             entry.facebook_id = body["originalRequest"]["data"]["sender"]["id"]
             entry.save()
             return HttpResponse(status=200)
         else:
             try:
-                profile_entry = Profile(user=user, facebook_id = body["originalRequest"]["data"]["sender"]["id"])
+                print("1")
+                profile_entry = Profile(user=user, facebook_id=body["originalRequest"]["data"]["sender"]["id"])
+                print("2")
                 profile_entry.save()
+                print("3")
                 return HttpResponse(status=200)            
             except:
                 return HttpResponse("Internal server error", status=500)
